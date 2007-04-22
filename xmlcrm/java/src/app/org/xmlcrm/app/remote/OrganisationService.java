bf9861a6584f10898c4bc55b4904c0aa8c824e0e
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2007-04-22 20:20:42 +0000

    some methods for editing organisations

--- xmlcrm/java/src/app/org/xmlcrm/app/remote/OrganisationService.java
+++ xmlcrm/java/src/app/org/xmlcrm/app/remote/OrganisationService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.xmlcrm.app.data.basic.Sessionmanagement;
+import org.xmlcrm.app.data.beans.basic.SearchResult;
 import org.xmlcrm.app.data.user.Usermanagement;
 import org.xmlcrm.app.data.user.Organisationmanagement;
 
@@ -17,10 +18,42 @@ public class OrganisationService {
 	 * @param SID
 	 * @return
 	 */
-	public List getOrganisations(String SID){
+	public SearchResult getOrganisations(String SID, int start ,int max, String orderby){
         int users_id = Sessionmanagement.getInstance().checkSession(SID);
         long USER_LEVEL = Usermanagement.getInstance().getUserLevelByID(users_id);
-        return Organisationmanagement.getInstance().getOrganisations(USER_LEVEL);
+        return Organisationmanagement.getInstance().getOrganisations(USER_LEVEL,start,max,orderby);
+	}
+	
+	
+	
+	/**
+	 * @deprecated
+	 * @param SID
+	 * @param orgname
+	 * @return
+	 */
+	public Long addOrganisation(String SID, String orgname){
+        int users_id = Sessionmanagement.getInstance().checkSession(SID);
+        long USER_LEVEL = Usermanagement.getInstance().getUserLevelByID(users_id);
+        return Organisationmanagement.getInstance().addOrganisation(USER_LEVEL, orgname, users_id);
+	}
+	
+	/**
+	 * adds or updates an Organisation
+	 * @param SID
+	 * @param organisation_id
+	 * @param orgname
+	 * @return
+	 */
+	public Long saveOrUpdateorganisation(String SID, long organisation_id, String orgname){
+        int users_id = Sessionmanagement.getInstance().checkSession(SID);
+        long USER_LEVEL = Usermanagement.getInstance().getUserLevelByID(users_id);
+        if (organisation_id==0){
+        	return Organisationmanagement.getInstance().addOrganisation(USER_LEVEL, orgname, users_id);
+        } else {
+        	return Organisationmanagement.getInstance().updateOrganisation(USER_LEVEL, organisation_id, orgname, users_id);
+        }
+        
 	}
 
 
