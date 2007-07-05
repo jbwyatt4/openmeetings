1d33f921faaa71e81aec926998c9c29c31ce5c65
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2007-07-05 11:37:55 +0000

    Fixes Windows version with missing batch Files

---
+++ branches/dev/xmlcrm/java/webapp/xmlcrm/jod/jodconverter.bat
@@ -0,0 +1,1 @@
+%1 -cp .;%4commons-io-1.2.jar;%4commons-logging-1.1.jar;%4jodconverter-2.1.1.jar;%4juh-2.1.0.jar;%4jurt-2.1.0.jar;%4ridl-2.1.0.jar;%4unoil-2.1.0.jar;%4xstream-1.1.3.jar com.artofsolving.jodconverter.tools.ConvertDocument %2 %3
