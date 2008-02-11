a04c61ab703588980cedb4d740a4b745c6437a09
togawamanabu <togawamanabu@gmail.com>
2008-02-11 14:33:33 +0000

    enhancement rlzxdoc tools

---
+++ trunk/St01/laszlo/client/rlzxdoc/LzxFile.rb
@@ -0,0 +1,46 @@
+class LzxFile
+  attr_reader :class_list
+  @locale
+
+  def initialize(path, locale)
+    @class_list = Array.new
+    @locale = locale
+
+    createlist(path)
+  end
+
+  def createlist(path)
+    source = File.read path
+    parser = Parsers::PullParser.new(source)
+
+    docsource_path = "#{File.dirname(File.expand_path(path))}/doc/lzx/#{@locale}/#{File.basename(path, ".*")}_doc.xml"
+    if File.exist?(docsource_path)
+      docxml = Document.new(File.new(docsource_path))
+    else
+      docxml = nil
+    end
+
+    lzxclass = nil
+    pre_comment = ""
+
+    while parser.has_next?
+      res = parser.pull
+
+      case res.event_type
+        when :comment
+          pre_comment = res[0]
+          pre_comment = pre_comment[1..-1] if pre_comment[0, 1] == "-"
+        when :start_element
+          tag = res[0]
+          name = res[1]["name"]
+
+          case tag
+            when "class"
+              @class_list << lzxclass = LzxClass.new(name, pre_comment, res[1]["extends"])
+              lzxclass.doc = XPath.first(docxml, "/rlzxdoc/class[@name='#{lzxclass.classname}']/doc") unless docxml.nil?
+          end
+        pre_comment = ""
+      end
+    end
+  end
+end
--- trunk/St01/laszlo/client/rlzxdoc/class_name_creater.rb
+++ trunk/St01/laszlo/client/rlzxdoc/class_name_creater.rb
@@ -1,7 +1,7 @@
 #/opt/local/bin/ruby
 
 #
-# lzxdoc build tool
+# lzx class name converter
 #
 # togawamanabu <togawamanabu@gmail.com>
 #
@@ -32,7 +32,7 @@ end
 @basedir = ARGV[0] || "."
 
 #outputdir for lzxdoc
-@outputdir = ARGV[1] || "doc"
+@outputdir = ARGV[1] || "class_names"
 
 #exceptdirs list from lzxdoc
 exceptdirs_input = ARGV[2] || "doc"
@@ -54,9 +54,6 @@ expand_outputdir = File.expand_path(@outputdir)
 FileUtils.rm_r(expand_outputdir) if File.exist?(expand_outputdir)
 Dir.mkdir(expand_outputdir)
 
-FileUtils.cp("#{@rlzxdoc_root}/styles.css", expand_outputdir)
-FileUtils.cp("#{@rlzxdoc_root}/index.html", expand_outputdir)
-
 Dir::chdir(@basedir)
 
 Dir::glob("*.lzx").each{ |lzxfile| add_class_from_lzx(lzxfile) }
@@ -69,31 +66,5 @@ Dir::glob("**/").each { |dir|
   end
 }
 
-@nav = Hash.new
-
-class_erb = File.open(@rlzxdoc_root + '/class.rhtml') {|f| ERB.new(f.read)}
-
-@lzx_classes.each{ |lzxclass|
-  html_output_dir = "#{expand_outputdir}/#{File.dirname(lzxclass[:path])}"
-  FileUtils.mkdir_p(html_output_dir) unless File.exist?(html_output_dir)
-
-  @lzxclass = lzxclass[:class]
-  unless @lzxclass.nil?
-    unless @lzxclass.private?
-      File.open("#{html_output_dir}/#{@lzxclass.classname}.html", "w"){ |file| file.puts class_erb.result(binding) }
-
-      url = "#{File.dirname(lzxclass[:path])}/#{@lzxclass.classname}.html"
-      category = File.dirname(lzxclass[:path])
-      @nav.store(category, Array.new) unless @nav.key?(category)
-      @nav[category] << {:title => @lzxclass.classname, :url => url}
-      puts "create: #{lzxclass[:path]}"
-    end
-  end
-}
-
-#create nav html
-nav_erb = File.open(@rlzxdoc_root + '/nav.rhtml') {|f| ERB.new(f.read)}
-File.open("#{expand_outputdir}/nav.html", "w"){ |file| file.puts nav_erb.result(binding)}
-
-#clean up empty dirs
-#Dir::glob("**/").each { |dir| Dir.rmdir(dir) if Dir.entries(dir).join == "..."}
+nav_erb = File.open(@rlzxdoc_root + '/class_name_csv.erb') {|f| ERB.new(f.read, nil, '%')}
+File.open("#{expand_outputdir}/class_name.csv", "w"){ |file| file.puts nav_erb.result(binding)}
