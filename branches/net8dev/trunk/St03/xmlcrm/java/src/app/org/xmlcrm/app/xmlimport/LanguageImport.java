1bd0ef9eafe7921a49f34e0dc2c2e206242b58a4
togawamanabu@gmail.com <togawamanabu@gmail.com@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2008-03-04 16:21:10 +0000

    import converted lzx classes and fix installer classes in java

---
+++ branches/net8dev/trunk/St03/xmlcrm/java/src/app/org/xmlcrm/app/xmlimport/LanguageImport.java
@@ -0,0 +1,78 @@
+package org.xmlcrm.app.xmlimport;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import org.xmlcrm.app.data.basic.Fieldmanagment;
+import org.xmlcrm.app.data.basic.Languagemanagement;
+import org.xmlcrm.app.hibernate.beans.lang.FieldLanguage;
+import org.xmlcrm.app.hibernate.beans.lang.Fieldvalues;
+import org.xmlcrm.app.hibernate.beans.lang.Fieldlanguagesvalues;
+
+public class LanguageImport {
+	private static final Log log = LogFactory.getLog(LanguageImport.class);
+	
+	public LanguageImport() {	}
+
+	private static LanguageImport instance = null;
+
+	public static synchronized LanguageImport getInstance() {
+		if (instance == null) {
+			instance = new LanguageImport();
+		}
+		return instance;
+	}	
+	
+	public Long addLanguageByDocument(Long language_id, InputStream is) throws Exception {
+		
+		//return null if no language availible
+		if (Languagemanagement.getInstance().getFieldLanguageById(language_id)==null) {
+			return null;
+		}
+		
+		
+		SAXReader reader = new SAXReader();
+        Document document = reader.read(is);
+        
+        Element root = document.getRootElement();
+        
+        for (Iterator i = root.elementIterator(); i.hasNext(); ) {
+        	Element itemObject = (Element) i.next();
+        	Long fieldvalues_id = Long.valueOf(itemObject.attribute("id").getText()).longValue();
+        	String fieldName = itemObject.attribute("name").getText();
+        	String value = itemObject.element("value").getText();
+        	log.error("CHECK "+language_id+","+fieldvalues_id+","+fieldName+","+value);
+        	this.addFieldValueById(language_id, fieldvalues_id, fieldName, value);
+        }
+        
+        return null;        
+	}
+	
+	private void addFieldValueById(Long language_id, Long fieldvalues_id, String fieldName, String value) throws Exception {
+		
+		Fieldvalues fv = Fieldmanagment.getInstance().getFieldvaluesById(fieldvalues_id);
+		
+		if (fv==null) {
+			Fieldmanagment.getInstance().addFieldById(fieldName, fieldvalues_id);
+		}
+		
+		Fieldlanguagesvalues flv = Fieldmanagment.getInstance().getFieldByIdAndLanguage(fieldvalues_id, language_id);
+		
+		if (flv==null) {
+			Fieldmanagment.getInstance().addFieldValueByFieldAndLanguage(fieldvalues_id, language_id, value);
+		} else {
+			flv.setValue(value);
+			flv.setUpdatetime(new java.util.Date());
+			Fieldmanagment.getInstance();
+			Fieldmanagment.getInstance().updateFieldValueByFieldAndLanguage(flv);
+		}
+	}
+	
+	
+}
---
+++ branches/net8dev/trunk/St03/xmlcrm/java/src/app/org/xmlcrm/utils/mail/MailHandler.java
@@ -0,0 +1,113 @@
+package org.xmlcrm.utils.mail;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+
+import java.util.*;
+
+import javax.activation.*;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.xmlcrm.app.data.basic.Configurationmanagement;
+
+/**
+ * 
+ * @author swagner
+ * 
+ */
+public class MailHandler {
+
+	private static final Log log = LogFactory.getLog(MailHandler.class);
+
+	public MailHandler() {
+	}
+
+	/**
+	 * send mail to address
+	 * 
+	 * @param toEmail
+	 * @param subj
+	 * @param message
+	 * @return
+	 */
+	public static String sendMail(String toEmail, String subj, String message) {
+		try {
+
+			// String smtpServer="smtp.xmlcrm.org";
+			String smtpServer = Configurationmanagement.getInstance().getConfKey(3, "smtp_server").getConf_value();
+			String smtpPort = Configurationmanagement.getInstance().getConfKey(3, "smtp_port").getConf_value();
+			String to = toEmail;
+			// String from = "openmeetings@xmlcrm.org";
+			String from = Configurationmanagement.getInstance().getConfKey(3,"system_email_addr").getConf_value();
+			String subject = subj;
+			String body = message;
+
+			return send(smtpServer, smtpPort, to, from, subject, body);
+		} catch (Exception ex) {
+			log.error("[sendMail] " ,ex);
+			return "Error: " + ex;
+		}
+	}
+
+	/**
+	 * Sending a mail with given values
+	 * 
+	 * @param smtpServer
+	 * @param to
+	 * @param from
+	 * @param subject
+	 * @param body
+	 * @return
+	 */
+	public static String send(String smtpServer, String smtpPort, String to, String from,
+			String subject, String body) {
+		try {
+
+			System.out.println("Message sending in progress");
+
+			Properties props = System.getProperties();
+
+			// -- Attaching to default Session, or we could start a new one --
+			//smtpPort 25 or 587
+			props.put("mail.smtp.host", smtpServer);
+			props.put("mail.smtp.port", smtpPort);
+			props.put("mail.smtp.auth", "true");
+
+			Session session = Session.getDefaultInstance(props,
+					new SmtpAuthenticator());
+
+			// -- Create a new message --
+			Message msg = new MimeMessage(session);
+
+			// -- Set the FROM and TO fields --
+			msg.setFrom(new InternetAddress(from));
+			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(
+					to, false));
+
+			// -- We could include CC recipients too --
+			// if (cc != null)
+			// msg.setRecipients(Message.RecipientType.CC
+			// ,InternetAddress.parse(cc, false));
+
+			// -- Set the subject and body text --
+			msg.setSubject(subject);
+			msg.setDataHandler(new DataHandler(new ByteArrayDataSource(body
+					.toString(), "text/html")));
+			// msg.setContent(body, "text/html");
+
+			// -- Set some other header information --
+			msg.setHeader("X-Mailer", "XML-Mail");
+			msg.setSentDate(new Date());
+
+			// -- Send the message --
+			Transport.send(msg);
+
+			return "success";
+		} catch (Exception ex) {
+			log.error("[mail send] " ,ex);
+			return "Error" + ex;
+		}
+	}
+	
+}
