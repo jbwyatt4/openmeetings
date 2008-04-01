911e6fd09b97767cdf3b3fd9e0715299bf67f635
mdokuko <mdokuko@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2008-04-01 13:07:34 +0000

    Fixes: Issue 370,371 Change to be able to execute command, even if the command or the parameters contain space.
     * Runtime#exec(String) --> Runtime#exec(String[])
     * Padding file separator at the end of ImageMagic path(if there is not file separator)
     * About PDF generating, using jodconverter directly, instead of calling batch(or shell) file

--- trunk/webapp/src/app/org/xmlcrm/app/documents/GenerateThumbs.java
+++ trunk/webapp/src/app/org/xmlcrm/app/documents/GenerateThumbs.java
@@ -37,9 +37,15 @@ public class GenerateThumbs {
 			String name = f.getName();
 			String folder = f.getParentFile().getAbsolutePath()+File.separatorChar;
 			
-			String command = current_dir + "jod" + File.separatorChar
-					+ runtimeFile + " " + filepath + ".jpg " + folder
-					+ pre+name+".jpg "+thumbSize;
+//			String command = current_dir + "jod" + File.separatorChar
+//					+ runtimeFile + " " + filepath + ".jpg " + folder
+//					+ pre+name+".jpg "+thumbSize;
+			String[] command = new String[4];
+			command[0] = current_dir + "jod" + File.separatorChar + runtimeFile;
+			command[1] = filepath + ".jpg";
+			command[2] = folder	+ pre+name+".jpg";
+			command[3] = String.valueOf(thumbSize);
+
 			returnMap.put("command", command);
 			Process proc = rt.exec(command);
 			InputStream stderr = proc.getErrorStream();
@@ -73,11 +79,22 @@ public class GenerateThumbs {
 			Runtime rt = Runtime.getRuntime();
 
 			String pathToIMagick = Configurationmanagement.getInstance().getConfKey(3,"imagemagick_path").getConf_value();
+			if(!pathToIMagick.endsWith(File.separator)){
+				pathToIMagick = pathToIMagick + File.separator;
+			}
 			
-			String command = current_dir + "jod" + File.separatorChar
-					+ runtimeFile + " " + inputfile + " " + outputpath
-					+ "_thumb_pages-%03d.jpg "+thumbSize + " "
-					+ pathToIMagick;
+//			String command = current_dir + "jod" + File.separatorChar
+//					+ runtimeFile + " " + inputfile + " " + outputpath
+//					+ "_thumb_pages-%03d.jpg "+thumbSize + " "
+//					+ pathToIMagick;
+			
+			String[] command = new String[5];
+			command[0] = current_dir + "jod" + File.separatorChar + runtimeFile;
+			command[1] = inputfile;
+			command[2] = outputpath	+ "_thumb_pages-%03d.jpg";
+			command[3] = String.valueOf(thumbSize);
+			command[4] = pathToIMagick;
+			
 			returnMap.put("command",command);
 			Process proc = rt.exec(command);
 			InputStream stderr = proc.getErrorStream();
