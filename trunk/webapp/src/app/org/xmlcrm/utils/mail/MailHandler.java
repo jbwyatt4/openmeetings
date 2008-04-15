b7b1c65d6757017a74fb36a19edab61d16b14915
mdokuko <mdokuko@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2008-04-15 02:15:02 +0000

    Fixes: Issue 207 If the email_username and email_userpassword configuration parameters are not defined, send method doesn't use SMTP authentication.

--- trunk/webapp/src/app/org/xmlcrm/utils/mail/MailHandler.java
+++ trunk/webapp/src/app/org/xmlcrm/utils/mail/MailHandler.java
@@ -42,26 +42,93 @@ public class MailHandler {
 			String from = Configurationmanagement.getInstance().getConfKey(3,"system_email_addr").getConf_value();
 			String subject = subj;
 			String body = message;
+			
+			String emailUsername = Configurationmanagement.getInstance().getConfKey(3, "email_username").getConf_value();
+			String emailUserpass = Configurationmanagement.getInstance().getConfKey(3, "email_userpass").getConf_value();
 
-			return send(smtpServer, smtpPort, to, from, subject, body);
+			//return send(smtpServer, smtpPort, to, from, subject, body);
+			return send(smtpServer, smtpPort, to, from, subject, body, emailUsername, emailUserpass);
 		} catch (Exception ex) {
 			log.error("[sendMail] " ,ex);
 			return "Error: " + ex;
 		}
 	}
 
+//	/**
+//	 * Sending a mail with given values
+//	 * 
+//	 * @param smtpServer
+//	 * @param to
+//	 * @param from
+//	 * @param subject
+//	 * @param body
+//	 * @return
+//	 */
+//	public static String send(String smtpServer, String smtpPort, String to, String from,
+//			String subject, String body) {
+//		try {
+//
+//			log.debug("Message sending in progress");
+//
+//			Properties props = System.getProperties();
+//
+//			// -- Attaching to default Session, or we could start a new one --
+//			//smtpPort 25 or 587
+//			props.put("mail.smtp.host", smtpServer);
+//			props.put("mail.smtp.port", smtpPort);
+//			props.put("mail.smtp.auth", "true");
+//
+//			Session session = Session.getDefaultInstance(props,
+//					new SmtpAuthenticator());
+//
+//			// -- Create a new message --
+//			Message msg = new MimeMessage(session);
+//
+//			// -- Set the FROM and TO fields --
+//			msg.setFrom(new InternetAddress(from));
+//			msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(
+//					to, false));
+//
+//			// -- We could include CC recipients too --
+//			// if (cc != null)
+//			// msg.setRecipients(Message.RecipientType.CC
+//			// ,InternetAddress.parse(cc, false));
+//
+//			// -- Set the subject and body text --
+//			msg.setSubject(subject);
+//			msg.setDataHandler(new DataHandler(new ByteArrayDataSource(body
+//					.toString(), "text/html")));
+//			// msg.setContent(body, "text/html");
+//
+//			// -- Set some other header information --
+//			msg.setHeader("X-Mailer", "XML-Mail");
+//			msg.setSentDate(new Date());
+//
+//			// -- Send the message --
+//			Transport.send(msg);
+//
+//			return "success";
+//		} catch (Exception ex) {
+//			log.error("[mail send] " ,ex);
+//			return "Error" + ex;
+//		}
+//	}
+	
 	/**
-	 * Sending a mail with given values
+	 * Sending a mail with given values.<br>
+	 * If the parameter "emailUsername" and "emailUserpass" is exist, use SMTP Authentication.
 	 * 
 	 * @param smtpServer
 	 * @param to
 	 * @param from
 	 * @param subject
 	 * @param body
+	 * @param emailUsername
+	 * @param emailUserpass
 	 * @return
 	 */
 	public static String send(String smtpServer, String smtpPort, String to, String from,
-			String subject, String body) {
+			String subject, String body, String emailUsername, String emailUserpass) {
 		try {
 
 			log.debug("Message sending in progress");
@@ -72,10 +139,19 @@ public class MailHandler {
 			//smtpPort 25 or 587
 			props.put("mail.smtp.host", smtpServer);
 			props.put("mail.smtp.port", smtpPort);
-			props.put("mail.smtp.auth", "true");
 
-			Session session = Session.getDefaultInstance(props,
-					new SmtpAuthenticator());
+			Session session = null;
+			if (emailUsername != null && emailUsername.length() > 0
+					&& emailUserpass != null && emailUserpass.length() > 0) {
+				//use SMTP Authentication
+				props.put("mail.smtp.auth", "true");
+				session = Session.getDefaultInstance(props,
+						new SmtpAuthenticator());
+			}else{
+				//not use SMTP Authentication
+				session = Session.getDefaultInstance(props, null);
+			}
+
 
 			// -- Create a new message --
 			Message msg = new MimeMessage(session);
