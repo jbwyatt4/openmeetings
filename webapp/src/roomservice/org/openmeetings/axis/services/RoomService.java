5a17ffe834b3a4b99f4917bc29f88e275bf49056
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2008-04-24 19:34:27 +0000

    Room-Services service.xml and added to Build-Instructions

---
+++ webapp/src/roomservice/org/openmeetings/axis/services/RoomService.java
@@ -0,0 +1,10 @@
+package org.openmeetings.axis.services;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class RoomService {
+	
+	private static final Log log = LogFactory.getLog(RoomService.class);
+
+}
