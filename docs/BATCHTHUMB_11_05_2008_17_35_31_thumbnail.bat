ba0abdc5296a53076a5f511649b439e62afe016c
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2008-05-14 16:11:58 +0000

    Sample Batch Scripts

--- docs/BATCHTHUMB_11_05_2008_17_35_31_thumbnail.bat
+++ docs/BATCHTHUMB_11_05_2008_17_35_31_thumbnail.bat
@@ -1,2 +1,2 @@
 convert -thumbnail 80 "C:\Programme\Red5 NT\webapps\openmeetings\upload\1\mama\mama.pdf" "C:\Programme\Red5 NT\webapps\openmeetings\upload\1\mama\_thumb_page.jpg"
-exit
+pause
--- docs/SWFCONVERT_11_05_2008_17_35_32_swfconverter.bat
+++ docs/SWFCONVERT_11_05_2008_17_35_32_swfconverter.bat
@@ -1,2 +1,2 @@
 pdf2swf "C:\Programme\Red5 NT\webapps\openmeetings\upload\1\mama\mama.pdf" "C:\Programme\Red5 NT\webapps\openmeetings\upload\1\mama\mama.swf"
-exit
+pause
