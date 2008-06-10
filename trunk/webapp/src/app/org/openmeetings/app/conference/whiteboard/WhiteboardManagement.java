eadf8418f5d5402484bb1370d2686de25b35a8a0
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2008-06-10 09:39:10 +0000

    Adds a Server-Side List of Whiteboard Objects

--- trunk/webapp/src/app/org/openmeetings/app/conference/whiteboard/WhiteboardManagement.java
+++ trunk/webapp/src/app/org/openmeetings/app/conference/whiteboard/WhiteboardManagement.java
@@ -1,9 +1,12 @@
 package org.openmeetings.app.conference.whiteboard;
 
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmeetings.app.remote.Application;
 
 public class WhiteboardManagement {
 	
@@ -22,9 +25,64 @@ public class WhiteboardManagement {
 	
 	public void addWhiteBoardObject(Long room_id, HashMap whiteboardObj) {
 		try {
+			log.debug("addWhiteBoardObject: "+whiteboardObj);
+			
+			log.debug("whiteboardObj 0: Event: "+whiteboardObj.get(0));
+			log.debug("whiteboardObj 1: Event: "+whiteboardObj.get(1));
+			log.debug("whiteboardObj 2: Event: "+whiteboardObj.get(2));
+			log.debug("whiteboardObj 3: Event: "+whiteboardObj.get(3));
+			
+			//log.debug("whiteboardObj NUMB3: Event: "+whiteboardObj.get(3).getClass().getName());
+			
+			Date dateOfEvent = (Date) whiteboardObj.get(1);
+			String action = whiteboardObj.get(2).toString();	
+			Map actionObject = (Map) whiteboardObj.get(3);
+			
+			log.debug("action: "+action);
+			
+			if (action.equals("draw") || action.equals("redo")){
+				HashMap<String,Map> roomList = Application.getWhiteBoardObjectList().get(room_id);
+				if (roomList == null) {
+					roomList = new HashMap<String,Map>();
+				}
+				
+				//log.debug(actionObject);
+				//log.debug(actionObject.size()-1);
+				//log.debug(actionObject.get(actionObject.size()-1));
+				
+				String objectOID = actionObject.get(actionObject.size()-1).toString();
+				log.debug("objectOID: "+objectOID);
+				roomList.put(objectOID, actionObject);
+				Application.setWhiteBoardObjectListRoomObj(room_id, roomList);
+			} else if (action.equals("clear")) {
+				HashMap<String,Map> roomList = Application.getWhiteBoardObjectList().get(room_id);
+				roomList = new HashMap<String,Map>();
+				Application.setWhiteBoardObjectListRoomObj(room_id, roomList);
+			} else if (action.equals("delete") && action.equals("undo")) {
+				HashMap<String,Map> roomList = Application.getWhiteBoardObjectList().get(room_id);
+				if (roomList == null) {
+					roomList = new HashMap<String,Map>();
+				}
+				String objectOID = actionObject.get(actionObject.size()-1).toString();
+				roomList.remove(objectOID);
+				Application.setWhiteBoardObjectListRoomObj(room_id, roomList);
+			} else if (action.equals("size") || action.equals("editProp") 
+					|| action.equals("editText")) {
+				HashMap<String,Map> roomList = Application.getWhiteBoardObjectList().get(room_id);
+				if (roomList == null) {
+					roomList = new HashMap<String,Map>();
+				}
+				String objectOID = actionObject.get(actionObject.size()-1).toString();
+				Map roomItem = roomList.get(objectOID);
+				roomList.put(objectOID, actionObject);
+				Application.setWhiteBoardObjectListRoomObj(room_id, roomList);
+			} else {
+				log.warn("Unkown Type: "+action+" actionObject: "+actionObject);
+			}
 			
 		} catch (Exception err) {
 			log.error("[addWhiteBoardObject]",err);
 		}
 	}
+	
 }
