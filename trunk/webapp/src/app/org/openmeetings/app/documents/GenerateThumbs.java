52f6b27c5b7e27ce817cc14daadce26f122664fd
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2009-01-06 15:44:39 +0000

    Fixes: Changes to MyISAM in Default MySQL storage engine because of better Performance

--- trunk/webapp/src/app/org/openmeetings/app/documents/GenerateThumbs.java
+++ trunk/webapp/src/app/org/openmeetings/app/documents/GenerateThumbs.java
@@ -111,7 +111,7 @@ public class GenerateThumbs {
 	}
 	
 	public HashMap<String,Object> generateBatchThumb(String current_dir, String inputfile, 
-			String outputpath, Integer thumbSize) {
+			String outputpath, Integer thumbSize, String pre) {
 		HashMap<String,Object> returnMap = new HashMap<String,Object>();
 		returnMap.put("process", "generateBatchThumb");			
 		try {
@@ -148,7 +148,88 @@ public class GenerateThumbs {
 			//Create the Content of the Converter Script (.bat or .sh File)
 			String fileContent = pathToIMagick + "convert -thumbnail " + thumbSize +
 					" " + "\"" + inputfile +"\"" +
-					" " + "\"" + outputpath+ "_thumb_page.jpg\"" +
+					" " + "\"" + outputpath+ "_"+ pre +"_page.jpg\"" +
+					Application.lineSeperator + "exit";
+				
+			//execute the Script
+			FileOutputStream fos = new FileOutputStream(executable_fileName);
+			fos.write(fileContent.getBytes());
+			fos.close();
+			
+			//make new shell script executable
+			//in JAVA6 this can be done directly through the api
+			if (System.getProperty("os.name").toUpperCase().indexOf("WINDOWS") == -1) {
+				MakeExectuable.getInstance().setExecutable(executable_fileName);
+			}
+
+			
+			Runtime rt = Runtime.getRuntime();
+			
+			for (int i=0;i<cmd.length;i++){
+				log.debug("cmd: "+cmd[i]);
+			}			
+			
+			returnMap.put("command", cmd.toString());
+			Process proc = rt.exec(cmd);
+			
+			InputStream stderr = proc.getErrorStream();
+			InputStreamReader isr = new InputStreamReader(stderr);
+			BufferedReader br = new BufferedReader(isr);
+			String line = null;
+			String error = "";
+			while ((line = br.readLine()) != null)
+				error += line;
+			returnMap.put("error", error);
+			int exitVal = proc.waitFor();
+			returnMap.put("exitValue", exitVal);
+			return returnMap;
+		} catch (Throwable t) {
+			t.printStackTrace();
+			returnMap.put("error", t.getMessage());
+			returnMap.put("exitValue", -1);
+			return returnMap;
+		}
+	}	
+	
+	public HashMap<String,Object> generateImageBatchByWidth(String current_dir, String inputfile, 
+			String outputpath, Integer thumbWidth, String pre) {
+		HashMap<String,Object> returnMap = new HashMap<String,Object>();
+		returnMap.put("process", "generateBatchThumb");			
+		try {
+			
+			String[] cmd;
+			String executable_fileName = "";
+			String pathToIMagick = Configurationmanagement.getInstance().getConfKey(3,"imagemagick_path").getConf_value();
+			if(!pathToIMagick.equals("") && !pathToIMagick.endsWith(File.separator)){
+				pathToIMagick = pathToIMagick + File.separator;
+			}
+
+			//If no Windows Platform
+			if (System.getProperty("os.name").toUpperCase().indexOf("WINDOWS") == -1) {
+				String runtimeFile = "thumbnail.sh";
+				executable_fileName = Application.batchFileFir+"BATCHTHUMB_" 
+						+ CalendarPatterns.getTimeForStreamId(new Date()) +"_"+ runtimeFile;
+		
+				cmd = new String[1];
+				cmd[0] = executable_fileName;
+			} else {
+				String runtimeFile = "thumbnail.bat";
+				executable_fileName = Application.batchFileFir+"BATCHTHUMB_" 
+						+ CalendarPatterns.getTimeForStreamId(new Date()) +"_"+ runtimeFile;
+				
+				cmd = new String[4];
+				cmd[0] = "cmd.exe";
+				cmd[1] = "/C";
+				cmd[2] = "start";
+				cmd[3] = executable_fileName;
+			}
+			log.debug("executable_fileName: "+executable_fileName);
+			
+			
+			//Create the Content of the Converter Script (.bat or .sh File)
+			String fileContent = pathToIMagick + "convert -resize " + thumbWidth +
+					" " + "\"" + inputfile +"\"" +
+					" " + "\"" + outputpath+ "_"+ pre +"_page.png\"" +
 					Application.lineSeperator + "exit";
 				
 			//execute the Script
