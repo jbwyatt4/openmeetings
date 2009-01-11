264512636b1af31caf28911a1b0bb57ce7c83207
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2009-01-11 16:42:27 +0000

    Fixes: Removal of some not needed Conversion

--- webapp/src/app/org/openmeetings/app/documents/GeneratePDF.java
+++ webapp/src/app/org/openmeetings/app/documents/GeneratePDF.java
@@ -77,15 +77,15 @@ public class GeneratePDF {
 			returnError.put("processOpenOffice", processOpenOffice);
 			HashMap<String,Object> processThumb = GenerateThumbs.getInstance().generateBatchThumb(current_dir, destinationFolder + fileName + ".pdf", destinationFolder, 80, "thumb");
 			returnError.put("processThumb", processThumb);		
-			HashMap<String,Object> processBig = GenerateThumbs.getInstance().generateImageBatchByWidth(current_dir, destinationFolder + fileName + ".pdf", destinationFolder, 660, "big");
-			returnError.put("processBig", processBig);		
+//			HashMap<String,Object> processBig = GenerateThumbs.getInstance().generateImageBatchByWidth(current_dir, destinationFolder + fileName + ".pdf", destinationFolder, 660, "big");
+//			returnError.put("processBig", processBig);		
 			HashMap<String,Object> processSWF = GenerateSWF.getInstance().generateSWF(current_dir, destinationFolder, destinationFolder, fileName);
 			returnError.put("processSWF", processSWF);	
 		} else {
 			HashMap<String,Object> processThumb = GenerateThumbs.getInstance().generateBatchThumb(current_dir, fileFullPath, destinationFolder, 80, "thumb");
 			returnError.put("processThumb", processThumb);
-			HashMap<String,Object> processBig = GenerateThumbs.getInstance().generateImageBatchByWidth(current_dir, fileFullPath, destinationFolder, 660, "big");
-			returnError.put("processBig", processBig);	
+//			HashMap<String,Object> processBig = GenerateThumbs.getInstance().generateImageBatchByWidth(current_dir, fileFullPath, destinationFolder, 660, "big");
+//			returnError.put("processBig", processBig);	
 			HashMap<String,Object> processSWF = GenerateSWF.getInstance().generateSWF(current_dir, (new File(fileFullPath)).getParentFile().getAbsolutePath()+File.separatorChar , destinationFolder, fileName);
 			returnError.put("processSWF", processSWF);				
 		}
