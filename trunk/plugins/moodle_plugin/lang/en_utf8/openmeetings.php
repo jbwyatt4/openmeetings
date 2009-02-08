77296efcaf82a06da80729c9e0fdb44f90dd7a6b
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2009-02-08 18:33:59 +0000

    Some new Moodle Modifications

---
+++ trunk/plugins/moodle_plugin/lang/en_utf8/openmeetings.php
@@ -0,0 +1,12 @@
+<?php
+
+$string['openmeetings'] = 'openmeetings';
+
+$string['modulename'] = 'OpenMeetings';
+$string['modulenameplural'] = 'OpenMeetings';
+
+$string['whiteboardfieldset'] = 'Custom example fieldset';
+$string['whiteboardintro'] = 'OpenMeetings Intro';
+$string['whiteboardname'] = 'Conference Name';
+
+?>
