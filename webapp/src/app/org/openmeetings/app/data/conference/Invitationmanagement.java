64b64b9d88eb18cbc1d3d5bd28cf019ce718d4de
o.becherer <o.becherer@medint.de>
2009-03-05 08:22:50 +0000

    Issue 625 : icalInvitation on saveAppointment

--- webapp/src/app/org/openmeetings/app/data/conference/Invitationmanagement.java
+++ webapp/src/app/org/openmeetings/app/data/conference/Invitationmanagement.java
@@ -1,7 +1,10 @@
 package org.openmeetings.app.data.conference;
 
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Vector;
 
 import org.slf4j.Logger;
 import org.red5.logging.Red5LoggerFactory;
@@ -11,15 +14,19 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.openmeetings.app.templates.InvitationTemplate;
 import org.openmeetings.app.data.basic.AuthLevelmanagement;
+import org.openmeetings.app.data.calendar.management.AppointmentLogic;
 import org.openmeetings.app.data.conference.Roommanagement;
 import org.openmeetings.app.data.user.Usermanagement;
 import org.openmeetings.app.data.user.dao.UsersDaoImpl;
 import org.openmeetings.app.data.basic.Configurationmanagement;
 import org.openmeetings.app.hibernate.beans.user.Users;
+import org.openmeetings.app.hibernate.beans.calendar.Appointment;
+import org.openmeetings.app.hibernate.beans.calendar.MeetingMember;
 import org.openmeetings.app.hibernate.beans.invitation.Invitations;
 import org.openmeetings.app.hibernate.utils.HibernateUtil;
 import org.openmeetings.utils.crypt.MD5;
 import org.openmeetings.utils.crypt.ManageCryptStyle;
+import org.openmeetings.utils.mail.IcalHandler;
 import org.openmeetings.utils.mail.MailHandler;
 
 /**
@@ -42,6 +49,24 @@ public class Invitationmanagement {
 		return instance;
 	}
 	
+	/**
+	 * Sending invitation within plain mail
+	 * @param user_level
+	 * @param username
+	 * @param message
+	 * @param baseurl
+	 * @param email
+	 * @param subject
+	 * @param rooms_id
+	 * @param conferencedomain
+	 * @param isPasswordProtected
+	 * @param invitationpass
+	 * @param valid
+	 * @param validFrom
+	 * @param validTo
+	 * @param createdBy
+	 * @return
+	 */
 	public String addInvitationLink(Long user_level, String username, String message,
 			String baseurl, String email, String subject, Long rooms_id, String conferencedomain,
 			Boolean isPasswordProtected, String invitationpass, Integer valid,
@@ -108,7 +133,98 @@ public class Invitationmanagement {
 			log.error("addInvitationLink",err);
 		}
 		return null;
+	}
+	
+	/**
+	 * @author o.becherer
+	 * @param user_level
+	 * @param username
+	 * @param message
+	 * @param baseurl
+	 * @param email
+	 * @param subject
+	 * @param rooms_id
+	 * @param conferencedomain
+	 * @param isPasswordProtected
+	 * @param invitationpass
+	 * @param valid
+	 * @param validFrom
+	 * @param validTo
+	 * @param createdBy
+	 * @return
+	 */
+	//---------------------------------------------------------------------------------------------------------
+	public String addInvitationIcalLink(Long user_level, String username, String message,
+			String baseurl, String email, String subject, Long rooms_id, String conferencedomain,
+			Boolean isPasswordProtected, String invitationpass, Integer valid,
+			Date validFrom, Date validTo, Long createdBy, Long appointMentId
+			){
+			log.debug("addInvitationIcalLink");
+			
+		try {
+			if (AuthLevelmanagement.getInstance().checkUserLevel(user_level)){
+				
+				Invitations invitation = new Invitations();
+				invitation.setIsPasswordProtected(isPasswordProtected);
+				if (isPasswordProtected){
+					invitation.setInvitationpass(ManageCryptStyle.getInstance().getInstanceOfCrypt().createPassPhrase(invitationpass));
+				}
+				
+				invitation.setInvitationWasUsed(false);
+				
+				//valid period of Invitation
+				if (valid == 1) {
+					//endless
+					invitation.setIsValidByTime(false);
+					invitation.setCanBeUsedOnlyOneTime(false);
+				} else if (valid == 2){
+					//period
+					invitation.setIsValidByTime(true);
+					invitation.setCanBeUsedOnlyOneTime(false);
+					invitation.setValidFrom(validFrom);
+					invitation.setValidTo(validTo);					
+				} else {
+					//one-time
+					invitation.setIsValidByTime(false);
+					invitation.setCanBeUsedOnlyOneTime(true);
+					invitation.setInvitationWasUsed(false);
+				}
+				
+				invitation.setDeleted("false");
+				
+				Users us = UsersDaoImpl.getInstance().getUser(createdBy);
+				String hashRaw = us.getLogin()+us.getUser_id()+(new Date());
+				invitation.setHash(MD5.do_checksum(hashRaw));
+				
+				invitation.setInvitedBy(us);
+				invitation.setInvitedname(username);
+				invitation.setInvitedEMail(email);
+				invitation.setRoom(Roommanagement.getInstance().getRoomById(rooms_id));
+				invitation.setConferencedomain(conferencedomain);
+				invitation.setStarttime(new Date());
+				
+				Object idf = HibernateUtil.createSession();
+				Session session = HibernateUtil.getSession();
+				Transaction tx = session.beginTransaction();
+				long invitationId = (Long) session.save(invitation);
+				tx.commit();
+				HibernateUtil.closeSession(idf);
+				
+				if (invitationId>0){
+					return this.sendInvitionIcalLink(username, message, baseurl, email, subject, invitation.getHash(), appointMentId, createdBy);
+				} else {
+					return "Sys - Error";
+				}
+			}
+		} catch (HibernateException ex) {
+			log.error("[addInvitationLink] "+ex);
+		} catch (Exception err){
+			log.error("addInvitationLink",err);
+		}
+		return null;
 	}	
+
+	//---------------------------------------------------------------------------------------------------------
 	
 	private String sendInvitionLink(String username, String message, 
 			String baseurl, String email, String subject, String invitationsHash){
@@ -127,7 +243,70 @@ public class Invitationmanagement {
 			log.error("sendInvitationLink",err);
 		}
 		return null;
-	}	
+	}
+	
+	/**
+	 * 
+	 * @param username
+	 * @param message
+	 * @param baseurl
+	 * @param email
+	 * @param subject
+	 * @param invitationsHash
+	 * @return
+	 */
+	private String sendInvitionIcalLink(String username, String message, 
+			String baseurl, String email, String subject, String invitationsHash, Long appointMentId, Long organizer_userId){
+		try {
+				
+			String invitation_link = baseurl+"?lzr=swf8&lzt=swf&invitationHash="+invitationsHash;
+			
+			Long default_lang_id = Long.valueOf(Configurationmanagement.getInstance().
+	        		getConfKey(3,"default_lang_id").getConf_value()).longValue();
+			
+			String template = InvitationTemplate.getInstance().getRegisterInvitationTemplate(username, message, invitation_link, default_lang_id);
+		
+			
+			IcalHandler handler = new IcalHandler(IcalHandler.ICAL_METHOD_REQUEST);
+			
+			
+			Appointment point = AppointmentLogic.getInstance().getAppointMentById(appointMentId);
+			
+			// Transforming Meeting Members
+			
+			HashMap<String, String> dusselInDerHashMap = handler.getAttendeeData(email, username);
+			
+			Vector<HashMap<String, String>> atts = new Vector<HashMap<String,String>>();
+			atts.add(dusselInDerHashMap);
+			
+			// Defining Organizer
+			Users user = Usermanagement.getInstance().getUserById(organizer_userId);
+			
+			HashMap<String, String> oberDussel = handler.getAttendeeData(user.getAdresses().getEmail(), user.getLogin());
+			
+			
+			GregorianCalendar start = new GregorianCalendar();
+			start.setTime(point.getAppointmentStarttime());
+			
+			GregorianCalendar end = new GregorianCalendar();
+			end.setTime(point.getAppointmentEndtime());
+			
+			
+			handler.addNewMeeting(start, end, point.getAppointmentName(), atts, invitation_link, oberDussel);
+			
+			System.out.println(handler.getICalDataAsString());
+			
+			MailHandler.sendIcalMessage(email, subject, handler.getIcalAsByteArray());
+			
+			
+			return "success";
+			//return MailHandler.sendMail(email, subject, template);
+
+		} catch (Exception err){
+			log.error("sendInvitionIcalLink",err);
+		}
+		return null;
+	}
 	
 	public String sendInvitionLink(Long user_level, String username, String message, String domain, String room, 
 			String roomtype, String baseurl, String email, String subject, Long room_id){
