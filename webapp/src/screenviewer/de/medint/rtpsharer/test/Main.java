aaf5b90b16beeca41935f78dcbb4029a34f1d24d
o.becherer <o.becherer@medint.de>
2009-06-15 08:24:10 +0000

    RTP Streaming ScreenSharer Basic Code (without GUI and no connection to openMeetings Backend)

---
+++ webapp/src/screenviewer/de/medint/rtpsharer/test/Main.java
@@ -0,0 +1,17 @@
+package de.medint.rtpsharer.test;
+
+import de.medint.rtpsharer.main.Streamer;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Streamer streamer = new Streamer();
+		streamer.start();
+
+	}
+
+}
