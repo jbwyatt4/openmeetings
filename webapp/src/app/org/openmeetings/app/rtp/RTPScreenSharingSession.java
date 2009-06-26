b85ea2025d30d0b8ddd53161443a0503726a900c
o.becherer <o.becherer@medint.de>
2009-06-26 14:51:59 +0000

    RTP Sharer -> velocity part for Viewer Applet

---
+++ webapp/src/app/org/openmeetings/app/rtp/RTPScreenSharingSession.java
@@ -0,0 +1,104 @@
+package org.openmeetings.app.rtp;
+
+import org.openmeetings.app.hibernate.beans.rooms.Rooms;
+import org.openmeetings.app.hibernate.beans.user.Users;
+
+/**
+ * represents a ScreenSharingSession within Conference
+ * @author o.becherer
+ *
+ */
+public class RTPScreenSharingSession {
+	
+	/** User sharing his Desktop */
+	private  Users sharingUser = null;
+	
+	/** Ip Address of sharing user */
+	private  String sharingIpAddress = "127.0.0.1";
+	
+	/** RTP Port incoming(Port the Shareres Client sends on)*/
+	private  int incomingRTPPort = 0;
+	
+	/** RTP Port outgoing (Port on which the RTP Stream is spread to single clients*/
+	private  int outgoingRTPPort = 0;
+	
+	/** Stream Width */
+	private  int streamWidth = 1024;
+	
+	/** StreamHeight */
+	private  int streamHeight = 768;
+	
+	/** ConferenceRoom */
+	private  Rooms room = null;
+	
+	/** JpegQuality */
+	private  float jpegQuality = 1f;
+
+	public Users getSharingUser() {
+		return sharingUser;
+	}
+
+	public void setSharingUser(Users sharingUser) {
+		this.sharingUser = sharingUser;
+	}
+
+	public String getSharingIpAddress() {
+		return sharingIpAddress;
+	}
+
+	public void setSharingIpAddress(String sharingIpAddress) {
+		this.sharingIpAddress = sharingIpAddress;
+	}
+
+	public int getIncomingRTPPort() {
+		return incomingRTPPort;
+	}
+
+	public void setIncomingRTPPort(int incomingRTPPort) {
+		this.incomingRTPPort = incomingRTPPort;
+	}
+
+	public int getOutgoingRTPPort() {
+		return outgoingRTPPort;
+	}
+
+	public void setOutgoingRTPPort(int outgoingRTPPort) {
+		this.outgoingRTPPort = outgoingRTPPort;
+	}
+
+	public int getStreamWidth() {
+		return streamWidth;
+	}
+
+	public void setStreamWidth(int streamWidth) {
+		this.streamWidth = streamWidth;
+	}
+
+	public int getStreamHeight() {
+		return streamHeight;
+	}
+
+	public void setStreamHeight(int streamHeight) {
+		this.streamHeight = streamHeight;
+	}
+
+	public Rooms getRoom() {
+		return room;
+	}
+
+	public void setRoom(Rooms room) {
+		this.room = room;
+	}
+
+	public float getJpegQuality() {
+		return jpegQuality;
+	}
+
+	public void setJpegQuality(float jpegQuality) {
+		this.jpegQuality = jpegQuality;
+	}
+	
+	
+	
+	
+}
--- webapp/src/app/org/openmeetings/app/rtp/RTPStreamingHandler.java
+++ webapp/src/app/org/openmeetings/app/rtp/RTPStreamingHandler.java
@@ -1,9 +1,89 @@
 package org.openmeetings.app.rtp;
 
-public class RTPStreamingHandler {
+import java.util.HashMap;
+import java.util.Iterator;
 
+import org.openmeetings.app.data.basic.Sessionmanagement;
+import org.openmeetings.app.data.conference.Roommanagement;
+import org.openmeetings.app.data.user.Usermanagement;
+import org.openmeetings.app.hibernate.beans.rooms.Rooms;
+import org.openmeetings.servlet.outputhandler.ScreenRequestHandler;
+import org.red5.logging.Red5LoggerFactory;
+import org.slf4j.Logger;
+
+public class RTPStreamingHandler {
+	
+	
+		private static final Logger log = Red5LoggerFactory.getLogger(ScreenRequestHandler.class, "openmeetings");
+	
+		/** Contains all RTPSessions*/
+		public static HashMap<Rooms, RTPScreenSharingSession> rtpSessions = new HashMap<Rooms, RTPScreenSharingSession>();
+
+		/** Minimal Limit for valid RTP Ports */
+		public static final int minimalRTPPort = 22220;
+		
+		/** Maximum for valid RTP Ports */
+		public static final int maximalRTPPort = 24000;
+		
+		/** Define The Port a Sharer should send his RTPStream on */
+		//---------------------------------------------------------------------------------------------
 		public static int getNextFreeRTPPort(){
-			//TODO : Define Range of valid ports and check, if another Session already uses it
-			return 22224;
+			log.debug("getNextFreeRTPPort");
+			
+			Iterator<Rooms> riter = rtpSessions.keySet().iterator();
+			
+			int currentPort = minimalRTPPort;
+			
+			if(rtpSessions.size() < 1)
+				return currentPort;
+			
+			
+			boolean portBlocked = true;
+			
+			while(portBlocked){
+			
+				while(riter.hasNext()){
+					Rooms r = riter.next();
+					RTPScreenSharingSession session = rtpSessions.get(r);
+				
+					if(session.getIncomingRTPPort() == currentPort){
+						portBlocked = true;
+						break;
+					}
+				}
+				
+				if(portBlocked)
+					currentPort ++;
+			}
+			
+			return currentPort;
+			
 		}
+		//---------------------------------------------------------------------------------------------
+		
+		/**
+		 * retrieving Session data for Room 
+		 */
+		public static RTPScreenSharingSession getSessionForRoom(String room, String sid) throws Exception{
+			log.debug("getSessionForRoom");
+			
+			if(room == null || room.length() <1)
+				throw new Exception("InputVal room not valid");
+			
+			Long users_id = Sessionmanagement.getInstance().checkSession(sid);
+			Long user_level = Usermanagement.getInstance().getUserLevelByID(users_id);
+
+		
+			Rooms myRoom= Roommanagement.getInstance().getRoomById(user_level, Long.parseLong(room));
+		
+			if(myRoom == null)
+				throw new Exception("no room available for ID " + room);
+			
+			if(!rtpSessions.containsKey(myRoom))
+				throw new Exception("no RTPSession available for Room " + room);
+		
+			return rtpSessions.get(myRoom);
+		}
+		
+		
 }
