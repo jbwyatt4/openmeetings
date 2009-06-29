6d67a928ed0172cbd46531915a65849fe9e8cdc7
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2009-06-29 06:49:02 +0000

    RTPStreamReceiver for Re-Streaming the RTP Stream - empty Sample

---
+++ webapp/src/app/org/openmeetings/app/rtp/RTPStreamReceiver.java
@@ -0,0 +1,35 @@
+package org.openmeetings.app.rtp;
+
+import javax.media.ControllerEvent;
+import javax.media.ControllerListener;
+import javax.media.rtp.ReceiveStreamListener;
+import javax.media.rtp.event.ReceiveStreamEvent;
+
+/**
+ * @author sebastianwagner
+ * 
+ * 
+ * Receives the Stream and Adds a new RTPManager 
+ * 
+ * it needs to be possible to have a method to addTarget and removeTarget from the RTPManager
+ *
+ */
+public class RTPStreamReceiver extends Thread implements ControllerListener, ReceiveStreamListener {
+
+	/* (non-Javadoc)
+	 * @see javax.media.ControllerListener#controllerUpdate(javax.media.ControllerEvent)
+	 */
+	public void controllerUpdate(ControllerEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.media.rtp.ReceiveStreamListener#update(javax.media.rtp.event.ReceiveStreamEvent)
+	 */
+	public void update(ReceiveStreamEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
