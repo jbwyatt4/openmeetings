a300f9e49f2a3c837b2acf6914928e93a429cd70
alexei.fedotov@gmail.com <alexei.fedotov@gmail.com@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2009-07-20 19:10:40 +0000

    Fixed compilation after r2180.

--- trunk/webapp/src/test/org/openmeetings/test/navi/TestNavi.java
+++ trunk/webapp/src/test/org/openmeetings/test/navi/TestNavi.java
@@ -8,7 +8,7 @@ import org.openmeetings.app.hibernate.beans.basic.Sessiondata;
 import org.openmeetings.app.hibernate.beans.user.Users;
 import org.openmeetings.app.remote.MainService;
 import org.openmeetings.app.hibernate.beans.basic.*;
-
+import org.openmeetings.app.hibernate.beans.domain.Organisation_Users;
 import junit.framework.TestCase;
 
 public class TestNavi extends TestCase {
@@ -26,26 +26,34 @@ public class TestNavi extends TestCase {
 		
 		System.out.println("us: "+us.getFirstname());
 		
-		List ll = mService.getNavi(sessionData.getSession_id(),1);
-		
-		System.out.println("NaviGlobal size: "+ll.size());
-		
-		for (Iterator it2 = ll.iterator(); it2.hasNext();) {
-			Naviglobal navigl = (Naviglobal) it2.next();
-			System.out.println(navigl.getLabel().getValue());
-			Set s = navigl.getMainnavi();
-			
-			for (Iterator it3 = s.iterator(); it3.hasNext();) {
-				Navimain navim = (Navimain) it3.next();
-				System.out.println("-->"+navim.getLabel().getValue());
-				
-				for (Iterator it4 = navim.getSubnavi().iterator(); it4.hasNext();) {
-					Navisub navis = (Navisub) it4.next();
-					System.out.println("---->"+navis.getLabel().getValue());
-				}			
-				
-			}			
-		}
+        for (Iterator<Organisation_Users> iter = us.getOrganisation_users()
+                .iterator(); iter.hasNext();) {
+
+            Long organization_id = iter.next().getOrganisation()
+                    .getOrganisation_id();
+            List ll = mService.getNavi(sessionData.getSession_id(), 1, organization_id);
+
+            System.out.println("NaviGlobal size: " + ll.size());
+
+            for (Iterator it2 = ll.iterator(); it2.hasNext();) {
+                Naviglobal navigl = (Naviglobal) it2.next();
+                System.out.println(navigl.getLabel().getValue());
+                Set s = navigl.getMainnavi();
+
+                for (Iterator it3 = s.iterator(); it3.hasNext();) {
+                    Navimain navim = (Navimain) it3.next();
+                    System.out.println("-->" + navim.getLabel().getValue());
+
+                    for (Iterator it4 = navim.getSubnavi().iterator(); it4
+                            .hasNext();) {
+                        Navisub navis = (Navisub) it4.next();
+                        System.out.println("---->"
+                                + navis.getLabel().getValue());
+                    }
+
+                }
+            }
+        }
 		
 	}
 
