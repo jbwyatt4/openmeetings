ec387c7379b40a2114395ea48bbaf481df18b444
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2009-12-07 20:40:43 +0000

    Fixes Issue 1032 Add French Moodle Translation contributed by Christian Theou

---
+++ plugins/moodle_plugin/lang/fr_utf8/openmeetings.php
@@ -0,0 +1,26 @@
+<?php
+
+$string['openmeetings'] = 'openmeetings';
+
+$string['modulename'] = 'OpenMeetings';
+$string['modulenameplural'] = 'OpenMeetings';
+
+$string['whiteboardfieldset'] = 'Custom example fieldset';
+$string['whiteboardintro'] = 'OpenMeetings Intro';
+$string['whiteboardname'] = 'Nom de la réunion';
+
+$string['Room Name'] = 'Nom R&eacute;union';
+$string['Room Type'] = 'Type R&eacute;union';
+$string['Room Language'] = 'Langue R&eacute;union';
+$string['Max User'] = 'Nb max utilisateurs';
+$string['Wait for teacher'] = 'Attendre enseignant';
+
+$string['Conference'] = 'R&eacute;union';
+$string['Audience'] = 'Conf&eacute;rence';
+$string['Restricted'] = 'Acc&egrave;s restreint';
+
+$string['Start with teacher'] = 'Les participants doivent attendre un enseignant pour pouvoir commencer';
+$string['Start without teacher'] = 'La r&eacute;union peut d&eacute;marrer imm&eacute;diatement (le premier connect&eacute; devient le mod&eacute;rateur)';
+
+
+?>
