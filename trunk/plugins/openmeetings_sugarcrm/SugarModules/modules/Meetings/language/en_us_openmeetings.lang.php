3d4eb0128d0d4afadf33b85ee068b332331ac953
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2010-03-03 15:53:21 +0000

    Plugin

---
+++ trunk/plugins/openmeetings_sugarcrm/SugarModules/modules/Meetings/language/en_us_openmeetings.lang.php
@@ -0,0 +1,15 @@
+<?php
+// created: 2008-06-06 11:53:13
+$mod_strings = array_merge($mod_strings,
+    array(
+ 'VALUE' => 'OPENMEETINGS meeting',
+  'LBL_IS_OPENMEETINGS' => 'Onlinemeeting',  
+  'LBL_OPENMEETINGS_ROOMNAME' => 'Access Room',    
+  'LBL_OPENMEETINGS_STARTADHOC' => 'My OpenMeetings Room', 
+       
+   )
+  )
+  ;
+
+
+?>
