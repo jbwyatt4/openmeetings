d7ddb3a460971b2e059cfee9f84bd3b44795332a
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2011-08-14 16:31:35 +0000

    Fixes: Thread and Taskexecutor for Mail Sending

--- injection/src/app/org/openmeetings/utils/mail/MailHandler.java
+++ injection/src/app/org/openmeetings/utils/mail/MailHandler.java
@@ -13,10 +13,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 public class MailHandler {
 
-	private static final Logger log = Red5LoggerFactory.getLogger(MailHandler.class, ScopeApplicationAdapter.webAppRootKey);
+	private static final Logger log = Red5LoggerFactory.getLogger(
+			MailHandler.class, ScopeApplicationAdapter.webAppRootKey);
 	@Autowired
 	private Configurationmanagement cfgManagement;
-	
+	@Autowired
+	private MailThread mailThread;
+
 	/**
 	 * send mail to address
 	 * 
@@ -29,51 +32,55 @@ public class MailHandler {
 		try {
 
 			// String smtpServer="smtp.xmlcrm.org";
-			String smtpServer = cfgManagement.getConfKey(3, "smtp_server").getConf_value();
-			String smtpPort = cfgManagement.getConfKey(3, "smtp_port").getConf_value();
+			String smtpServer = cfgManagement.getConfKey(3, "smtp_server")
+					.getConf_value();
+			String smtpPort = cfgManagement.getConfKey(3, "smtp_port")
+					.getConf_value();
 			String to = toEmail;
 			// String from = "openmeetings@xmlcrm.org";
-			String from = cfgManagement.getConfKey(3,"system_email_addr").getConf_value();
+			String from = cfgManagement.getConfKey(3, "system_email_addr")
+					.getConf_value();
 			String subject = subj;
 			String body = message;
-			
-			String emailUsername = cfgManagement.getConfKey(3, "email_username").getConf_value();
-			String emailUserpass = cfgManagement.getConfKey(3, "email_userpass").getConf_value();
 
-			//return send(smtpServer, smtpPort, to, from, subject, body);
-		
-			MailThread mailThread = new MailThread(smtpServer, smtpPort, to, from, subject, body, emailUsername, emailUserpass);
-			mailThread.start();
-			
+			String emailUsername = cfgManagement
+					.getConfKey(3, "email_username").getConf_value();
+			String emailUserpass = cfgManagement
+					.getConfKey(3, "email_userpass").getConf_value();
+
+			// return send(smtpServer, smtpPort, to, from, subject, body);
+
+			mailThread.doSend(smtpServer, smtpPort, to, from, subject, body,
+					emailUsername, emailUserpass);
+
 			return "success";
-			
+
 		} catch (Exception ex) {
-			log.error("[sendMail] " ,ex);
+			log.error("[sendMail] ", ex);
 			return "Error: " + ex;
 		}
 	}
 
-
-	
-	
-	
 	/**
 	 * @author o.becherer
-	 * @param recipients (List of valid mail adresses)
-	 * @param subject mailSubject
-	 * @param iCalMimeBody byte[] containing Icaldata
-	 * Sending Ical Invitation 
+	 * @param recipients
+	 *            (List of valid mail adresses)
+	 * @param subject
+	 *            mailSubject
+	 * @param iCalMimeBody
+	 *            byte[] containing Icaldata Sending Ical Invitation
 	 */
-	//---------------------------------------------------------------------------------------------
-	public static void sendIcalMessage(String recipients, String subject, byte[] iCalMimeBody, String htmlBody) throws Exception{
-		log.debug("sendIcalMessage");
-		
-		
-		MailiCalThread mailiCalThread = new MailiCalThread(recipients, subject, iCalMimeBody, htmlBody);
-		
-		mailiCalThread.start();
-		
-	}
-	//---------------------------------------------------------------------------------------------
-	
+	// ---------------------------------------------------------------------------------------------
+	// public static void sendIcalMessage(String recipients, String subject,
+	// byte[] iCalMimeBody, String htmlBody) throws Exception {
+	// log.debug("sendIcalMessage");
+	//
+	// MailiCalThread mailiCalThread = new MailiCalThread(recipients, subject,
+	// iCalMimeBody, htmlBody);
+	//
+	// mailiCalThread.start();
+	//
+	// }
+	// ---------------------------------------------------------------------------------------------
+
 }
