8bc74c8f6d27dc9b906e9b259c2cb8a1de9672d5
seba.wagner <seba.wagner@7b01d772-cd2b-0410-8f67-a1032f00e51c>
2011-09-30 15:49:19 +0000

    Fix config to hide password

--- plugins/moodle_plugin/settings.php
+++ plugins/moodle_plugin/settings.php
@@ -12,8 +12,8 @@ $settings->add(new admin_setting_configtext('openmeetings_red5port', get_string(
 $settings->add(new admin_setting_configtext('openmeetings_openmeetingsAdminUser', get_string('openmeetingsAdminUser', 'openmeetings'),
                        get_string('openmeetingsAdminUser', 'openmeetings'), "admin", PARAM_TEXT));
                        
-$settings->add(new admin_setting_configtext('openmeetings_openmeetingsAdminUserPass', get_string('openmeetingsAdminUserPass', 'openmeetings'),
-                       get_string('openmeetingsAdminUserPass', 'openmeetings'), "password", PARAM_PASSWORD));                       
+$settings->add(new admin_setting_configpasswordunmask('openmeetings_openmeetingsAdminUserPass', get_string('openmeetingsAdminUserPass', 'openmeetings'),
+                       get_string('openmeetingsAdminUserPass', 'openmeetings'), "password", PARAM_TEXT));                       
                    
 $settings->add(new admin_setting_configtext('openmeetings_openmeetingsModuleKey', get_string('openmeetingsModuleKey', 'openmeetings'),
                        get_string('openmeetingsModuleKey', 'openmeetings'), "moodle", PARAM_TEXT));
