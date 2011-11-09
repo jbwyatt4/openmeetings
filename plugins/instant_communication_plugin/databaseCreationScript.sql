85490df9928c230b008ecad50111212a0efd98bc
german.grekhov <german.grekhov@gmail.com>
2011-11-09 06:39:09 +0000

    Create a brunch for the separate video component

--- plugins/instant_communication_plugin/databaseCreationScript.sql
+++
@@ -1,4 +0,0 @@
-create database om_server;
-use om_server;
-create table logged_users ( user_hash varchar(160) primary key, last_present int(15), state int, user_data_escaped varchar(256), client_id varchar(100));
-create table calls ( from_id varchar(160), to_id varchar(160), last_updated int(15), state int);
