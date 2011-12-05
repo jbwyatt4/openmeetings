3753038f1e589d9c7feb8a25b0b84f4b5d7a7919
solomax666 <solomax666@gmail.com>
2011-12-05 15:37:03 +0000

    Tag for Release 1.9.0 is created

--- plugins/moodle_plugin/db/log.php
+++
@@ -1,9 +0,0 @@
-<?PHP
-
-defined('MOODLE_INTERNAL') || die();
-
-$logs = array(
-    array('module'=>'openmeetings', 'action'=>'add', 'mtable'=>'openmeetings', 'field'=>'name'),
-    array('module'=>'openmeetings', 'action'=>'update', 'mtable'=>'openmeetings', 'field'=>'name'),
-    array('module'=>'openmeetings', 'action'=>'view', 'mtable'=>'openmeetings', 'field'=>'name')
-);
